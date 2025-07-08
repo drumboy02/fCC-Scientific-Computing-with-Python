c17713187cdb5f50e8111855e649b64bac14a94b
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2025-07-08 13:42:36 -0400

    started build_a_probability_calculator.py project

---
+++ build-a-probability-calculator/build_a_probability_calculator.py
@@ -0,0 +1,19 @@
+import copy
+import random
+
+class Hat:
+    pass
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
+
+class Hat:
+    def __init__(self, **kwargs):
+        i = kwargs.items()
+        self.contents = [k for k, v in i for _ in range(v)]
+
+    def draw(self, nballs):
+        pass
+
+test = Hat(yellow=3, blue=2, green=6)
+print(test.contents)
