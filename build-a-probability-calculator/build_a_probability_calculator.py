9f278fbddee572a697d67e98326127d9e30a47c7
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2025-07-08 18:23:24 -0400

    added draw() method logic for drawing balls at random

--- build-a-probability-calculator/build_a_probability_calculator.py
+++ build-a-probability-calculator/build_a_probability_calculator.py
@@ -2,18 +2,32 @@ import copy
 import random
 
 class Hat:
-    pass
-
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
-
-class Hat:
     def __init__(self, **kwargs):
         i = kwargs.items()
         self.contents = [k for k, v in i for _ in range(v)]
 
     def draw(self, nballs):
-        pass
+        hballs = self.contents
+        res = []
+
+        if nballs > len(hballs):
+            for i in range(len(hballs)):
+                r = random.randrange(len(hballs))
+                res.append(hballs[r])
+                del hballs[r]
+
+            return res
+
+        for i in range(nballs):
+            r = random.randrange(len(hballs))
+            res.append(hballs[r])
+            del hballs[r]
+
+        return res
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
 
 test = Hat(yellow=3, blue=2, green=6)
-print(test.contents)
+# print(test.contents)
+print(test.draw(11))
